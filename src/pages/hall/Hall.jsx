dfc638698d144d1e0047eec02a8c97a4f4100bc7
aeikx <lkm200789@gmail.com>
2025-07-14 14:08:38 +0900

    back connect 4.0

--- src/pages/hall/Hall.jsx
+++ src/pages/hall/Hall.jsx
@@ -35,19 +35,18 @@ function Hall() {
             <div className="time">
               {() => {
                 let origin = new Date(item.clearTime);
-                let local = moment(origin.getMilliseconds)
+                return moment(origin.getMilliseconds)
                   .utcOffset("+09:00")
-                  .millisecond();
-                return new Date(local).toLocaleString("ko-KR", {
-                  timeZone: "Asia/Seoul",
-                  year: "numeric",
-                  month: "2-digit",
-                  day: "2-digit",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  second: "2-digit",
-                  hour12: false,
-                });
+                  .toLocaleString("ko-KR", {
+                    timeZone: "Asia/Seoul",
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                    hour12: false,
+                  });
               }}
             </div>
           </div>
